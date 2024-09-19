5bc2abc3f6d7792aa0ad237aee7d6ec25df0ce93
adhlira <adhliramadhan68@gmail.com>
2024-09-19 21:28:51 +0700

    add authorization

--- src/routes/books_route.js
+++ src/routes/books_route.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import books_controller from "../controllers/books_controller.js";
+import { Permission } from "../authorization.js";
+import { authToken, authorizePermission } from "../middleware.js";
 
 const router = new Router();
 const booksController = new books_controller();
 
-router.get("/books", (req, res) => booksController.getAllBook(req, res));
+router.use(authToken)
+
+router.get("/books", authorizePermission(Permission.BROWSE_BOOK), (req, res) => booksController.getAllBook(req, res));
 router.get("/books/:id", (req, res) => booksController.getBookById(req, res));
 router.post("/books", (req, res) => booksController.addNewBook(req, res));
 router.put("/books/:id", (req, res) => booksController.editBook(req, res));
